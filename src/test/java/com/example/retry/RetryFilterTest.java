e4af00ddb04b75bb782b57d8d91d33e3c8ad2f97
Berger Stefan <stefan.berger@x-tention.at>
2020-07-08 20:20:09 +0200

    Add rewriteFilter

--- src/test/java/com/example/retry/RetryFilterTest.java
+++ src/test/java/com/example/retry/RetryFilterTest.java
@@ -23,6 +23,8 @@ import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.RetryGatewayFilterFactory;
 import org.springframework.cloud.gateway.filter.factory.RetryGatewayFilterFactory.RetryConfig;
+import org.springframework.cloud.gateway.filter.factory.RewritePathGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.RewritePathGatewayFilterFactory.Config;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -41,12 +43,17 @@ public class RetryFilterTest {
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
       RetryGatewayFilterFactory factory = new RetryGatewayFilterFactory();
-      OrderedGatewayFilter filter = new OrderedGatewayFilter(factory.apply(new RetryConfig()
+      OrderedGatewayFilter retryFilter = new OrderedGatewayFilter(factory.apply(new RetryConfig()
           .setStatuses(HttpStatus.SERVICE_UNAVAILABLE).allMethods().setRetries(1).setSeries()), 1);
+
+      RewritePathGatewayFilterFactory rewriteFactory = new RewritePathGatewayFilterFactory();
+      OrderedGatewayFilter rewriteFilter = new OrderedGatewayFilter(
+          rewriteFactory.apply(new Config().setRegexp("/test").setReplacement("/test2")), 6);
 
       return builder.routes()
           .route(spec -> spec.path("/test").and().method(HttpMethod.GET)
-              .filters(f -> f.filter(filter)).uri("http://localhost:64324").id("retry-test"))
+              .filters(f -> f.filters(retryFilter, rewriteFilter)).uri("http://localhost:64324")
+              .id("retry-test"))
           .build();
     }
   }
@@ -56,16 +63,10 @@ public class RetryFilterTest {
 
   @Test
   public void testRetry() throws InterruptedException {
-    stubFor(get(urlEqualTo("/test")).willReturn(aResponse().withStatus(503)));
+    stubFor(get(urlEqualTo("/test2")).willReturn(aResponse().withStatus(503)));
 
     var response =
         webClient.get().uri("/test").header(HttpHeaders.CONTENT_TYPE, "text/xml").exchange();
     response.expectStatus().isEqualTo(503);
-
-    Thread.sleep(10000);
-
-    stubFor(get(urlEqualTo("/test")).willReturn(aResponse().withStatus(200)));
-    response = webClient.get().uri("/test").header(HttpHeaders.CONTENT_TYPE, "text/xml").exchange();
-    response.expectStatus().isEqualTo(200);
   }
 }
